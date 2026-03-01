3cadecd737bb0eb5b6ec1c519c92418eba617ad2
sa_malu <68854747+sa-malu@users.noreply.github.com>
2026-03-01 15:07:08 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
   TOKEN: process.env.TOKEN,
-  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID,
-  PUNISH_LOG_CHANNEL_ID: process.env.PUNISH_LOG_CHANNEL_ID,
-  SENESCAL_ID: process.env.SENESCAL_ID,
-  SENESCAL_FEED_CHANNEL_ID: process.env.SENESCAL_FEED_CHANNEL_ID,
+
+  CARGO_VERIFICADO: process.env.CARGO_VERIFICADO || "1476714100804554862",
+
+  LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID, // geral
+  VOICE_LOG_CHANNEL_ID: process.env.VOICE_LOG_CHANNEL_ID || null, // opcional
+  PUNISH_LOG_CHANNEL_ID: process.env.PUNISH_LOG_CHANNEL_ID, // punições Senescal
+
+  SENESCAL_ID: process.env.SENESCAL_ID || null,
+  SENESCAL_FEED_CHANNEL_ID: process.env.SENESCAL_FEED_CHANNEL_ID || null,
 };
